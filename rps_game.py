2823816f44538fef1b54cfacf2cf80579b954c2d
Shelduck <e.l.sheldon@icloud.com>
2022-03-24 20:51:38 +0100

    Tidies up the code.

--- rps_game.py
+++ rps_game.py
@@ -15,7 +15,6 @@ class RockPaperScissors:
         self.cap = cv2.VideoCapture(0)
         self.started = False
         self.countdown_switch = False
-        self.end = False
         self.camera_error = False
         self.timestamp = time.time()
         self.time_elapsed = 0
@@ -42,41 +41,41 @@ class RockPaperScissors:
     
     def intro(self):
         self.message_left = 'ROCK, PAPER, SCISSORS!'
-        self.message_left_mid = 'Press s to start.'
+        self.message_left_mid = 'Press \'s\' to start.'
+        self.switch_on()
+    
+    def switch_on(self):
         if cv2.waitKey(33) & 0xFF == ord('s'):
             self.started = True
             self.countdown_switch = True
-    
-    def game_switch(self):
+
+    def control_panel(self):
         if self.started == False:
             if self.round_marker == 1:
                 self.intro()
             elif self.round_marker == 4:
                 self.end_game()
             elif self.camera_error == True:
-                self.message_left_low = 'The camera can\'t detect you. Press s to try again.'
-                if cv2.waitKey(33) & 0xFF == ord('s'):
-                    self.started = True
-                    self.countdown_switch = True 
+                self.message_left_mid = 'The camera can\'t detect you. Press \'s\' to try again.'
+                self.switch_on()
             elif self.round_marker > 1:
                 self.message_left_low = f'Press \'s\' to move onto round {self.round_marker}.'
-                if cv2.waitKey(33) & 0xFF == ord('s'):
-                    self.started = True
-                    self.countdown_switch = True
+                self.switch_on()
             
 
     def get_player_choices(self):
         if self.started == True:
-            self.message_left_low = ''
             self.time_elapsed = (10 * self.round_marker) - (time.time() - self.timestamp)
+
             if self.countdown_switch:
                 self.message_left_mid = f'Show your hand in {int(self.time_elapsed)} seconds.'
             
             if self.time_elapsed <=0:
-                self.countdown_switch = False
                 self.message_left_low = ''
+                self.countdown_switch = False
                 self.computer_choice = random.choice(self.options[:3])
                 index = self.get_prediction()
+
                 if index == 3:
                     self.camera_error = True                
                 else:
@@ -84,6 +83,7 @@ class RockPaperScissors:
                     self.message_left = f'You chose {self.player_choice}.'
                     self.message_right = f'Computer chose {self.computer_choice}.'
                     self.determine_round_winner()
+
 
     def get_prediction(self):
         prediction = self.model.predict(self.data)
@@ -96,7 +96,6 @@ class RockPaperScissors:
             self.started = False
         else:
             self.compare_inputs()
-            self.time_elapsed = 0
             self.started = False
 
     def compare_inputs(self):
@@ -113,9 +112,7 @@ class RockPaperScissors:
             self.computer_points += 1
             self.message_left_mid = 'Computer wins this round!'
 
-
     def end_game(self):
-        self.message_left_mid = ''
         self.compare_inputs()
         if self.player_points == self.computer_points:
             self.message_left_low = 'It\'s a draw! Good game.'
@@ -125,13 +122,8 @@ class RockPaperScissors:
             self.message_left_low = 'You lost. Better luck next time!'
         self.message_left_low_low = 'Press q to close the game.'
         
-
-
-
     def close_window(self):
-        #print('before')
         self.cap.release()
-        #print('after')
         cv2.waitKey(1000)
         cv2.waitKey(1)
         cv2.waitKey(1000)
@@ -147,7 +139,7 @@ def play_game():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         game.play_video()
-        game.game_switch()
+        game.control_panel()
         game.get_player_choices()
     game.close_window()
     
