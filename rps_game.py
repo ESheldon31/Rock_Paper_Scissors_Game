a8803f29fc93256ea34a4d81174928da27e21f63
Shelduck <e.l.sheldon@icloud.com>
2022-03-24 18:29:56 +0100

    Adds countdown

--- rps_game.py
+++ rps_game.py
@@ -5,95 +5,145 @@ import random
 import time
 
 
-class Rock_paper_scissors:
-    def __init__(self): 
+class RockPaperScissors:
+    def __init__(self):
         self.round_marker = 1
         self.options = ['rock', 'paper', 'scissors', 'nothing']
         self.player_points = 0
         self.computer_points = 0
         self.model = load_model('keras_model.h5')
         self.cap = cv2.VideoCapture(0)
-        print('Get ready!')
+        self.started = False
+        self.countdown_switch = False
+        self.end = False
+        self.timestamp = time.time()
+        self.time_elapsed = 0
+        self.message_left = ''
+        self.message_right = ''
+        self.message_left_mid = ''
+        self.message_left_low = ''
+        self.message_left_low_low = ''
         pass
-
-    def compare_input(self, player_choice, computer_choice):
-        time.sleep(2)
-        if player_choice == computer_choice:
-            self.player_points += 1
-            self.computer_points += 1
-            self.round_marker += 1
-            print('It\'s a draw!')
-        elif (player_choice == 'scissors' and computer_choice == 'paper') \
-            or (player_choice == 'paper' and computer_choice == 'rock') \
-            or (player_choice == 'rock' and computer_choice == 'scissors'):
-            self.player_points += 1
-            self.round_marker += 1
-            print('You win this round!')
-        else: 
-            self.computer_points += 1
-            self.round_marker += 1
-            print('You lose this round!')
-        if self.round_marker <4:
-            time.sleep(1)
-            print(f'Get ready for round {self.round_marker}.')
-
-    def get_prediction(self):
-        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-        ret, frame = self.cap.read()
-        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+    
+    def play_video(self):
+        self.data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        ret, self.frame = self.cap.read()
+        resized_frame = cv2.resize(self.frame, (224, 224), interpolation = cv2.INTER_AREA)
         image_np = np.array(resized_frame)
         normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-        data[0] = normalized_image
-        prediction = self.model.predict(data)
-        cv2.imshow('frame', frame)
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-            #return None
+        self.data[0] = normalized_image
+        cv2.putText(self.frame, self.message_left, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 3)
+        cv2.putText(self.frame, self.message_right, (430, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 3)
+        cv2.putText(self.frame, self.message_left_mid, (10, 350), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 3)
+        cv2.putText(self.frame, self.message_left_low, (10, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 3)
+        cv2.putText(self.frame, self.message_left_low_low, (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 3)
+        cv2.imshow('frame', self.frame)
+    
+    def intro(self):
+        self.message_left = 'ROCK, PAPER, SCISSORS!'
+        self.message_left_mid = 'Press s to start.'
+        if cv2.waitKey(33) & 0xFF == ord('s'):
+            self.started = True
+            self.countdown_switch = True
+    
+    def game_switch(self):
+        if self.started == False:
+            if self.round_marker == 1:
+                self.intro()
+            elif self.round_marker == 4:
+                self.end_game()
+            elif self.round_marker > 1:
+                self.message_left_low = f'Press \'s\' to move onto round {self.round_marker}.'
+                if cv2.waitKey(33) & 0xFF == ord('s'):
+                    self.started = True
+                    self.countdown_switch = True
+
+    def get_player_choices(self):
+        if self.started == True:
+            self.message_left_low = ''
+            self.time_elapsed = (10 * self.round_marker) - (time.time() - self.timestamp)
+            if self.countdown_switch:
+                self.message_left_mid = f'Show your hand in {int(self.time_elapsed)} seconds.'
+            
+            if self.time_elapsed <=0:
+                self.countdown_switch = False
+                self.message_left_low = ''
+                self.computer_choice = random.choice(self.options[:3])
+                index = self.get_prediction()
+                if index == 3:
+                    self.message_left_low = 'The camera can\'t detect you. Press s to try again.'
+                else:
+                    self.player_choice = self.options[index]
+                    self.message_left = f'You chose {self.player_choice}.'
+                    self.message_right = f'Computer chose {self.computer_choice}.'
+                    self.determine_round_winner()
+
+    def get_prediction(self):
+        prediction = self.model.predict(self.data)
         index = np.argmax(prediction[0])
         return index
+    
+    def determine_round_winner(self):
+        self.round_marker += 1
+        if self.round_marker == 4:
+            self.started = False
+        else:
+            self.compare_inputs()
+            self.time_elapsed = 0
+            self.started = False
 
-    def get_input(self):
-        while True:
-            start_key = input('Press enter to start.')
-            if start_key == '':
-                computer_choice = random.choice(self.options[:3])
-                while True: 
-                    time.sleep(1)
-                    index = self.get_prediction()
-                    if index == 3:
-                        print('The camera can\'t see you. Try again')
-                    else:
-                        player_choice = self.options[index]
-                        print(f'You chose {player_choice}.')
-                        time.sleep(1)
-                        print('The computer chose...')
-                        time.sleep(2)
-                        print(computer_choice + ' .')
-                        self.compare_input(player_choice, computer_choice)
-                        break 
-            break
+    def compare_inputs(self):
+        if self.player_choice == self.computer_choice:
+            self.player_points += 1
+            self.computer_points += 1
+            self.message_left_mid = 'It\'s a draw!'
+        elif (self.player_choice == 'scissors' and self.computer_choice == 'paper') \
+            or (self.player_choice == 'paper' and self.computer_choice == 'rock') \
+            or (self.player_choice == 'rock' and self.computer_choice == 'scissors'):
+            self.player_points += 1
+            self.message_left_mid = 'You win this round!'
+        else: 
+            self.computer_points += 1
+            self.message_left_mid = 'Computer wins this round!'
+
+
+    def end_game(self):
+        self.message_left_mid = ''
+        self.compare_inputs()
+        if self.player_points == self.computer_points:
+            self.message_left_low = 'It\'s a draw! Good game.'
+        elif self.player_points > self.computer_points:
+            self.message_left_low = 'You won the game! Well done!'
+        else: 
+            self.message_left_low = 'You lost. Better luck next time!'
+        self.message_left_low_low = 'Press q to close the game.'
+        
+
+
 
     def close_window(self):
-        # After the loop release the cap object
+        #print('before')
         self.cap.release()
-        # Destroy all the windows
+        #print('after')
+        cv2.waitKey(1000)
+        cv2.waitKey(1)
+        cv2.waitKey(1000)
+        cv2.waitKey(1)
         cv2.destroyAllWindows()
+        cv2.waitKey(1)
+        cv2.waitKey(1)
 
+    
 def play_game():
-    game = Rock_paper_scissors()
-    while True:
-        game.get_input()
-        if game.round_marker == 4:
-            time.sleep(2)
-            if game.player_points == game.computer_points:
-                print('It\'s a draw! Good game.')
-                break
-            elif game.player_points > game.computer_points:
-                print('You won the game! Well done!')
-                break
-            else: print('You lost. Better luck next time!')
-            game.close_window()
+    game = RockPaperScissors()
+    while game.round_marker < 5:
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        
-            
+        game.play_video()
+        game.game_switch()
+        game.get_player_choices()
+    game.close_window()
+    
+
 if __name__ == '__main__':
     play_game()
